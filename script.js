d923f4fe238df96325e1639c6a769e74465dfcac
Gagik <gagikpog@gmail.com>
2018-07-19 17:48:33 +0500

    optimization

--- script.js
+++ script.js
@@ -1,51 +1,43 @@
-var map,mapt;
-var W = 250,H = 120;
+var map1 =[],map2 =[];
+var W = 100,H = 50;
 var pause = true;
+var activemap1 = true;
 var iter = 0;
 
-createMap(W,H);
-initWindow(W,H);
+createmap1(map1);
+createmap1(map2);
+initWindow();
 randomFilling();
 renderMap();
 timer();
 
 
 function createCustomElement(elementClass, elementID,tagName){
-  var aTag = document.createElement(tagName);
-  aTag.id = elementID;
-  aTag.className = elementClass;
-  return aTag ;
+ 	var aTag = document.createElement(tagName);
+	aTag.id = elementID;
+	aTag.className = elementClass;
+	aTag.onclick = onClickMap;
+	//aTag.addEventListener("click",onClickMap);
+	return aTag ;
 }
 
-function createMap(w,h){
-	createMapt(w,h);
-	map = [];
-	for (var i = 0; i < h; i++){
-	   map[i] = [];
-	    for (var j = 0; j < w; j++){
-	        map[i][j] = 0;
+function createmap1(_map1){
+	for (var i = 0; i < H; i++){
+	   _map1[i] = [];
+	    for (var j = 0; j < W; j++){
+	        _map1[i][j] = false;
 		}
 	}
 }
 
-function createMapt(w,h){
-	mapt = [];
-	for (var i = 0; i < h; i++){
-	   mapt[i] = [];
-	    for (var j = 0; j < w; j++){
-	        mapt[i][j] = 0;
-		}
-	}
-}
-
-function initWindow(w,h){
+function initWindow(){
 	var parentDoc = document.getElementById('tbl');
-	for (var i = 0 ; i < h; i++)
+	for (var i = 0 ; i < H; i++)
 	{
 		var customElementRow = createCustomElement("row", "row"+(i+1),"tr");
 		parentDoc.appendChild(customElementRow);
 		var parent = document.getElementById("row"+(i+1));
-		for (var j = 0 ; j < w; j++)
+		for (var j = 0 ; j < W; j++)
 		{
 			var customElement = createCustomElement("col", "col"+(i+1)+"_"+(j+1),"td");
 			parent.appendChild(customElement);
@@ -54,73 +46,67 @@ function initWindow(w,h){
 }
 
 function renderMap(){
+	var _map1 = activemap1? map1:map2;
 	for(var i = 0;i<H;i++){
 		for(var j = 0;j<W;j++){
-			if(map[i][j])
-				document.getElementById("col"+(i+1)+"_"+(j+1)).style.backgroundColor = "white";
-			else document.getElementById("col"+(i+1)+"_"+(j+1)).style.backgroundColor = "#202020";
+			document.getElementById("col"+(i+1)+"_"+(j+1)).style.backgroundColor = _map1[i][j] ? "white" : "#202020";
 		}
 	}
 }
 
 function randomFilling(){
 	iter = 0;
+	var _map1 = activemap1? map1:map2;
 	var rand = function (min, max) {
  		return Math.random() * (max - min) + min;
 	}
 	for(var i = 0;i<H;i++){
 		for(var j = 0;j<W;j++){
-			map[i][j] = Math.floor(rand(0,2));
+			_map1[i][j] = !Math.floor(rand(0,2));
 		}
 	}
 }
 
-
 function CountNeighbors(y,x){
+	var _map1 = activemap1? map1:map2;
 	var res = 0;
 	for (var i = - 1; i <= 1; i++)
 	{
 		for (var j = - 1; j <= 1; j++)
 		{
-			var y1 = (y + i + H) % H;
-			var x1 = (x + j + W) % W;
-			if (map[y1][x1])
+			if (_map1[(y + i + H) % H][(x + j + W) % W])
 				res++;
 		}
 	}
-	if (map[y][x])
+	if (_map1[y][x])
 		res--;
 	return res;
 }
 
 function nextGeneration()
 {
+	var _map1 = activemap1 ? map1 : map2;
+	var _map2 = activemap1 ? map2 : map1;
 	for(var i = 0;i<H;i++){
 		for(var j = 0;j<W;j++){
 			var n =  CountNeighbors(i,j);
-			mapt[i][j] = (n == 3) || ((n == 2) && (map[i][j]));
+			_map2[i][j] = (n == 3) || ((n == 2) && (_map1[i][j]));
 		}
 	}
 
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
-			map[i][j] = mapt[i][j];
-		}
-	}
+	activemap1 = !activemap1;
 	iter++;	
 	renderMap();
 }
 
 function timer() {
 	if(pause){
-	nextGeneration();
-	setTimeout(timer, 5);
-	var aTag = document.getElementById("floor");
-	aTag.innerText = "Generation: "+iter;
-
+		nextGeneration();
+		setTimeout(timer, 5);
+		var aTag = document.getElementById("floor");
+		aTag.innerText = "Generation: "+iter;
 	}
 }
-
 
 function btmPause()
 {
@@ -128,5 +114,33 @@ function btmPause()
 	aTag.value = pause ? "Play":"Pause"
 	pause = !pause
 	timer();
+}
 
+function onClickMap(objThis)
+{
+	var name = objThis.currentTarget.id;
+	if (~name.indexOf("row")) {
+		return;
+	}
+
+	var y = name.substr(3,name.indexOf("_")-3)-1;
+	var x = name.substr(name.indexOf("_")+1)-1;
+
+	var _map1 = activemap1 ? map1 : map2;
+	_map1[y][x] = true;
+	renderMap();
+
+	//console.log(objThis.currentTarget.id);
+}
+
+function clearMap()
+{
+	iter = 0;
+	var _map1 = activemap1 ? map1 : map2;
+	for(var i = 0;i<H;i++){
+		for(var j = 0;j<W;j++){
+			_map1[i][j] = false;
+		}
+	}
+	renderMap();
 }
