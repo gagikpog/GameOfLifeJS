b10160620dfce1457f83a7466c906d980d6cf89e
Gagik <gagikpog@gmail.com>
2019-06-29 20:51:18 +0500

    draw on canvas

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var map1 =[],map2 =[];
-var W = 150,H = 70;
-var pause = true;
-var activemap1 = true;
-var iter = 0;
-var cursorPressed = false;
-var editMode = true;
-var penSize = 1;
-var cursorPos = {X:10,Y:10}
+let map1 = [], map2 = [];
+const W = 150, H = 70;
+const SIZE = 10;
+let pause = true;
+let activemap1 = true;
+let iter = 0;
+let cursorPressed = false;
+let editMode = true;
+let penSize = 1;
+let cursorPos = { X: 10, Y: 10 }
+let canvas = document.getElementById("tbl");
+let ctx = canvas.getContext("2d");
+let canvasMargin = { X: 0, Y: 0 };
 
 document.addEventListener("keydown", keyDownFunc, false);
 createmap1(map1);
@@ -15,78 +19,95 @@ initWindow();
 randomFilling();
 renderMap();
 timer();
+window.onresize = resize;
+resize();
 
-function createCustomElement(elementClass, elementID,tagName){
- 	var aTag = document.createElement(tagName);
-	aTag.id = elementID;
-	aTag.className = elementClass;
-	aTag.onclick = onClickMap;
-	aTag.onmousemove = onMouseMoveMap;
-	return aTag ;
-}
-
-function createmap1(_map1){
-	for (var i = 0; i < H; i++){
-	   _map1[i] = [];
-	    for (var j = 0; j < W; j++){
-	        _map1[i][j] = false;
+/**
+ * @description при изменении размера экрана обновляет значение 
+ * отступов для определения позиции курсора на canvas
+ * @param {Event} event 
+ */
+function resize(event) {
+	canvasMargin.Y = canvas.offsetTop;
+	canvasMargin.X = canvas.offsetLeft;
+};
+/**
+ * @description создает матрицу
+ * @param {[]} _map1
+ */
+function createmap1(_map1) {
+	for (let i = 0; i < H; i++) {
+		_map1[i] = [];
+		for (let j = 0; j < W; j++) {
+			_map1[i][j] = false;
 		}
 	}
 }
-
-function initWindow(){
-	var parentDoc = document.getElementById('tbl');
-	for (var i = 0 ; i < H; i++)
-	{
-		var customElementRow = createCustomElement("row", "row"+(i+1),"tr");
-		parentDoc.appendChild(customElementRow);
-		var parent = document.getElementById("row"+(i+1));
-		for (var j = 0 ; j < W; j++)
-		{
-			var customElement = createCustomElement("col", "col"+(i+1)+"_"+(j+1),"td");
-			parent.appendChild(customElement);
-		}
-	}
+/**
+ * @description дает размеры canvas при загрузке страници
+ */
+function initWindow() {
+	canvas.width = SIZE * W;
+	canvas.height = SIZE * H;
 }
-
-function renderMap(){
-	var _map1 = activemap1? map1:map2;
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
-			document.getElementById("col"+(i+1)+"_"+(j+1)).style.backgroundColor = _map1[i][j] ? "white" : "#202020";
+/**
+ * @description рисует точку
+ * @param {Integer} x позиция x
+ * @param {Integer} y позиция y
+ * @param {String} col цвет
+ */
+function drawPixel(x, y, col) {
+	ctx.fillStyle = col;
+	ctx.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+}
+/**
+ * @description выводит текущее состояние матрицы на экран
+ */
+function renderMap() {
+	//ссылка на активную матрицу
+	let _map1 = activemap1 ? map1 : map2;
+	let i = 0, j = 0;
+	//отрисовка поля 
+	for (i = 0; i < H; i++) {
+		for (j = 0; j < W; j++) {
+			drawPixel(j, i, _map1[i][j] ? "white" : "#202020");
 		}
 	}
-	for	(var i = parseInt(-(penSize-1)/2); i < 1 + parseInt((penSize)/2); i++){
-		for(var j = parseInt(-(penSize-1)/2); j < 1 + parseInt((penSize)/2); j++){
-			if(i+1+cursorPos.Y<1||i+1+cursorPos.Y>H||j+1+cursorPos.X<1||j+1+cursorPos.X>W)
+	//рисуем курсор рисования 
+	for (i = parseInt(-(penSize - 1) / 2); i < 1 + parseInt((penSize) / 2); i++) {
+		for (j = parseInt(-(penSize - 1) / 2); j < 1 + parseInt((penSize) / 2); j++) {
+			if (i + 1 + cursorPos.Y < 1 || i + 1 + cursorPos.Y > H || j + 1 + cursorPos.X < 1 || j + 1 + cursorPos.X > W)
 				continue;
-			var col  = document.getElementById("col"+(i+1+cursorPos.Y)+"_"+(j+1+cursorPos.X));
-			var str = "#" + col.style.backgroundColor;
-			col.style.backgroundColor = str == "#rgb(32, 32, 32)" ? "#606060": "#b0b0b0";
+			drawPixel(j + cursorPos.X, i + cursorPos.Y, _map1[i + cursorPos.Y][j + cursorPos.X] ? '#fff' : '#555');
 		}
-	}	
+	}
 }
-
-function randomFilling(){
+/**
+ * @description заполняет матрицу случайным образом
+ */
+function randomFilling() {
 	iter = 0;
-	var _map1 = activemap1? map1:map2;
-	var rand = function (min, max) {
- 		return Math.random() * (max - min) + min;
+	let _map1 = activemap1 ? map1 : map2;
+	let rand = function (min, max) {
+		return Math.random() * (max - min) + min;
 	}
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
-			_map1[i][j] = !Math.floor(rand(0,2));
+	for (let i = 0; i < H; i++) {
+		for (let j = 0; j < W; j++) {
+			_map1[i][j] = !Math.floor(rand(0, 2));
 		}
 	}
 }
-
-function CountNeighbors(y,x){
-	var _map1 = activemap1? map1:map2;
-	var res = 0;
-	for (var i = - 1; i <= 1; i++)
-	{
-		for (var j = - 1; j <= 1; j++)
-		{
+/**
+ * @description считает сколько соседних клеток активные
+ * @param {Integer} y 
+ * @param {Integer} x 
+ * @returns {Integer} возвращает количество активных клеток
+ */
+function CountNeighbors(y, x) {
+	let _map1 = activemap1 ? map1 : map2;
+	let res = 0;
+	for (let i = - 1; i <= 1; i++) {
+		for (let j = - 1; j <= 1; j++) {
 			if (_map1[(y + i + H) % H][(x + j + W) % W])
 				res++;
 		}
@@ -95,101 +116,108 @@ function CountNeighbors(y,x){
 		res--;
 	return res;
 }
-
-function nextGeneration()
-{
-	var _map1 = activemap1 ? map1 : map2;
-	var _map2 = activemap1 ? map2 : map1;
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
-			var n =  CountNeighbors(i,j);
+/**
+ * @description рассчитывает состояние следующего поколения
+ */
+function nextGeneration() {
+	let _map1 = activemap1 ? map1 : map2;
+	let _map2 = activemap1 ? map2 : map1;
+	for (let i = 0; i < H; i++) {
+		for (let j = 0; j < W; j++) {
+			let n = CountNeighbors(i, j);
 			_map2[i][j] = (n == 3) || ((n == 2) && (_map1[i][j]));
 		}
 	}
-
 	activemap1 = !activemap1;
-	iter++;	
+	iter++;
 }
-
+/**
+ * @description таемер
+ */
 function timer() {
-	if(pause){
+	if (pause) {
 		nextGeneration();
-		var aTag = document.getElementById("floor");
-		aTag.innerText = "Generation: "+iter;
+		let aTag = document.getElementById("floor");
+		aTag.innerText = "Generation: " + iter;
 	}
 	renderMap();
 	setTimeout(timer, 5);
 }
-
-function btmPause()
-{
-	var aTag = document.getElementById("pause");
-	aTag.value = pause ? "Play":"Pause"
+/**
+ * @description обработчика события кнопки “Пауза”
+ */
+function btmPause() {
+	let aTag = document.getElementById("pause");
+	aTag.value = pause ? "Play" : "Pause"
 	pause = !pause
-	//timer();
 }
+/**
+ * @description обработчик события нажатия на canvas
+ * @param {Event} objThis 
+ */
+function onClickMap(objThis) {
+	let y = cursorPos.Y;
+	let x = cursorPos.X;
 
-function onClickMap(objThis)
-{
-	var name = objThis.currentTarget.id;
-	if (~name.indexOf("row")) {
-		return;
-	}
-
-	var y = name.substr(3,name.indexOf("_")-3)-1;
-	var x = name.substr(name.indexOf("_")+1)-1;
-
-	var _map1 = activemap1 ? map1 : map2;
-//	_map1[y][x] = editMode;
-	cursorPos.X = x;
-	cursorPos.Y = y;
-	for	(var i = parseInt(-(penSize-1)/2); i < 1 + parseInt((penSize)/2); i++){
-		for(var j = parseInt(-(penSize-1)/2); j < 1 + parseInt((penSize)/2); j++){
-			_map1[i+y][j+x] = editMode;
+	let _map1 = activemap1 ? map1 : map2;
+	for (let i = parseInt(-(penSize - 1) / 2); i < 1 + parseInt((penSize) / 2); i++) {
+		for (let j = parseInt(-(penSize - 1) / 2); j < 1 + parseInt((penSize) / 2); j++) {
+			_map1[i + y][j + x] = editMode;
 		}
-	}	
+	}
 	renderMap();
 }
-
-function onMouseMoveMap(objThis)
-{
+/**
+ * @description обработчик события - движение мыши на  canvas
+ * @param {Event} objThis
+ */
+function onMouseMoveMap(objThis) {
+	mMove(objThis);
 	if (cursorPressed) {
 		onClickMap(objThis);
 	}
-	mMove(objThis);
 }
-
-function clearMap()
-{
+/**
+ * @description очищает матрицу
+ */
+function clearMap() {
 	iter = 0;
-	var _map1 = activemap1 ? map1 : map2;
-	for(var i = 0;i<H;i++){
-		for(var j = 0;j<W;j++){
+	let _map1 = activemap1 ? map1 : map2;
+	for (let i = 0; i < H; i++) {
+		for (let j = 0; j < W; j++) {
 			_map1[i][j] = false;
 		}
 	}
 	renderMap();
 }
-
-function mouseDown(objThis)
-{
+/**
+ * @description нажатие мыши на canvas
+ * @param {Event} objThis 
+ */
+function mouseDown(objThis) {
 	cursorPressed = true;
+	onMouseMoveMap(objThis);
 }
-
-function mouseUp(objThis)
-{
+/**
+ * //отжатие мыши на canvas 
+ * @param {Event} objThis 
+ */
+function mouseUp(objThis) {
 	cursorPressed = false;
 }
-
-function changeMode()
-{
-	var tImg = document.getElementById("mode");
-	tImg.src = editMode ? "img/eraser.png":"img/pencil.png";
+/**
+ * @description меняет ластик на карандаш и обратно
+ */
+function changeMode() {
+	let tImg = document.getElementById("mode");
+	tImg.src = editMode ? "img/eraser.png" : "img/pencil.png";
 	editMode = !editMode;
 }
-
-function keyDownFunc(e)
-{
+/**
+ * @description нажатие на клавиатуре
+ * @param {Event} e
+ */
+function keyDownFunc(e) {
 	/*key p*/
 	if (e.keyCode == 80) {
 		if (!editMode) {
@@ -203,20 +231,19 @@ function keyDownFunc(e)
 		}
 	}
 }
-
-function penSizeChange()
-{
+/**
+ * @description меняет размер кисти
+ */
+function penSizeChange() {
 	penSize = document.getElementById("pencilSize").value;
 }
-
-function mMove(objThis){
-	var name = objThis.currentTarget.id;
-	if (~name.indexOf("row")) {
-		return;
+/**
+ * @description движение мыши в нажатом состоянии
+ * @param {Event} objThis 
+ */
+function mMove(objThis) {
+	if (objThis) {
+		cursorPos.X = parseInt((objThis.clientX - canvasMargin.X) / SIZE);
+		cursorPos.Y = parseInt((objThis.clientY - canvasMargin.Y) / SIZE);
 	}
-
-	var y = name.substr(3,name.indexOf("_")-3)-1;
-	var x = name.substr(name.indexOf("_")+1)-1;
-	cursorPos.X = x;
-	cursorPos.Y = y;
 }
